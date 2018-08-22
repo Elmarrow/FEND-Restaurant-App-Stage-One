12570368b85ffe744ff0f9cf29b9ce66f81a2ea8
Aggelos Afratis <aafratis@gmail.com>
2018-08-22 12:07:16 +0300

    Added new token for MAPBOX API

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoibWFnZ290YnJhaW4iLCJhIjoiY2psNHd5YXRxMjlodDN2b2QxbXozMW85MyJ9.edMHG0hvXKVmx0VfJwV6cA',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
